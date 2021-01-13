c6ab12433e2517f59e05989f226b22d0fa7870ab
Nikita-arc <70189966+Nikita-arc@users.noreply.github.com>
2021-01-13 20:35:33 +0300

    Create pyth.py

---
+++ Algoitmika learn/Test/pyth.py
@@ -0,0 +1,11 @@
+def Python_guru(python):
+    if python == "python_start" or python == "python_pro":
+        return True
+    else:
+        return False
+    
+teacher = input("Enter corses")
+if Python_guru(python=int):
+    print("yes")
+else:
+    print("no")
