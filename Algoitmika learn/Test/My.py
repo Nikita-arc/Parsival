d7e04aae5eee9d21f6bc214b57aaf12d67ff5e0a
Nikita-arc <70189966+Nikita-arc@users.noreply.github.com>
2021-01-21 10:33:16 +0300

    Update My.py

--- Algoitmika learn/Test/My.py
+++ Algoitmika learn/Test/My.py
@@ -1,13 +1,26 @@
-def new_func():
-    def Python_guru(python):
-        if python == "python_start" or python == "python_pro":
+class Teacher:
+ 
+    def __init__(self, name, cours):
+        self.name = name 
+        self.cours = cours
+ 
+    def python_guru (self, cours):
+        if cours == 'Python Start' or cours == 'Python Pro':
             return True
-        else:
-            return False
-    teacher = input("Enter corses")
-    if Python_guru:
-        print("yes")
-    else:
-        print("no")
-
-return new_func()
+        return False
+ 
+    def __str__(self):
+        return self.name
+ 
+list_teacher = []
+ 
+n = int(input('Введите количество преподавателей'))
+for i in range(n):
+    name = input("Введите имя преподавателя")
+    cours = input('Введите список курсов через запятую').split(',')
+    teacher = Teacher(name, cours)
+    list_teacher.append(teacher)
+ 
+for teacher in list_teacher:
+    if teacher.python_guru():
+        print ("Гуру python", teacher)
