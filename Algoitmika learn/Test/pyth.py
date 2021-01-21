f05d853e109c1fcb1259929632c2fb016212620f
Nikita-arc <70189966+Nikita-arc@users.noreply.github.com>
2021-01-21 10:33:25 +0300

    Update pyth.py

--- Algoitmika learn/Test/pyth.py
+++ Algoitmika learn/Test/pyth.py
@@ -1,11 +1,8 @@
-def Python_guru(python):
-    if python == "python_start" or python == "python_pro":
-        return True
-    else:
-        return False
-    
-teacher = input("Enter corses")
-if Python_guru(python=int):
-    print("yes")
-else:
-    print("no")
+def perimeter(a,b):
+    return 2*(a+b)
+def area(a,b):
+    return a*b
+a = int(input("Enter the first side of the rectangle: "))
+b = int(input("Enter the second side of the rectangle: "))
+print('Perimeter = ',perimeter(a,b))
+print('Area = ',area(a,b))
