7c771b8d93d9328da4c2108fc8c8aa0d7f3c966a
Nikita Kinstler <kadet2000nikita@gmail.com>
2021-04-28 18:27:48 +0700

    Delete program2.py

--- Algoitmika learn/Задачи/program2.py
+++
@@ -1,8 +0,0 @@
-a = b = 1
-c = input('Введите номер искомого элемента : ')
-c = int(c)
-for n in range(int(c-2)):
-    tmp = a + b
-    a = b
-    b = tmp
-print(str(c)+' элемент последовательности равен ' + str(b))
