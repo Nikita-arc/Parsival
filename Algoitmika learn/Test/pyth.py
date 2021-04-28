1e6bd41ba5b8b3b2e4cb15b457b0629ce7f484ec
Nikita Kinstler <kadet2000nikita@gmail.com>
2021-04-28 18:27:42 +0700

    Delete pyth.py

--- Algoitmika learn/Test/pyth.py
+++
@@ -1,8 +0,0 @@
-def perimeter(a,b):
-    return 2*(a+b)
-def area(a,b):
-    return a*b
-a = int(input("Enter the first side of the rectangle: "))
-b = int(input("Enter the second side of the rectangle: "))
-print('Perimeter = ',perimeter(a,b))
-print('Area = ',area(a,b))
