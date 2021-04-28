cd92b52c44d37328e6e8383bd03ab4a06b729baa
Nikita Kinstler <kadet2000nikita@gmail.com>
2021-04-28 18:23:22 +0700

    28.04.21, 18:23

---
+++ learn/expression-2.0.py
@@ -0,0 +1,7 @@
+lenght = int(input('Введите первую сторону треугольника: '))
+breadth = int(input('Введите втоую сторону треугольника: '))
+
+area = lenght + breadth
+
+print('Периметр равен: ', area)
+print('Площадь равна: ', 2 * area)
